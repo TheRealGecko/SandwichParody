fc2445248fd6df79a81a03d4c5ceb6e693b4ddd4
purple-affogato <110414458+purple-affogato@users.noreply.github.com>
2023-04-08 18:21:50 -0400

    items now appear :D
    they just don't stop appearing tho

--- stage_1.gd
+++ stage_1.gd
@@ -1,1 +1,23 @@
 extends Node
+
+func _ready():
+	pass
+
+
+func _on_teeth_timer_timeout():
+	var nodes = get_tree().get_nodes_in_group("teeth")
+	var node = nodes[randi() % nodes.size()]
+	var pos = node.position
+	var teeth = preload("res://nail_polish_teeth.tscn").instantiate()
+	add_child(teeth)
+	teeth.position = pos
+
+
+func _on_bling_timer_timeout():
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	var randomX = rng.randi_range(180, 800)
+	var randomY = rng.randi_range(30, 570)
+	var bling = preload("res://bling.tscn").instantiate()
+	add_child(bling)
+	bling.position = Vector2(randomX, randomY)
