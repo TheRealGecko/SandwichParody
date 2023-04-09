fe554c5bcebb42ada985bab00b5fc16f8ed88b57
purple-affogato <110414458+purple-affogato@users.noreply.github.com>
2023-04-08 21:40:00 -0400

    teeth fixed

--- nail_polish_teeth.gd
+++ nail_polish_teeth.gd
@@ -1,1 +1,10 @@
-extends RigidBody2D
+extends Area2D
+
+var speed = 100
+
+func _process(delta):
+	global_position.x -= speed * delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
