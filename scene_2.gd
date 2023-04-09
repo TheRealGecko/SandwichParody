c0f31d9771d43fea0f3bcba218fa3e0511cc319b
purple-affogato <110414458+purple-affogato@users.noreply.github.com>
2023-04-09 13:16:43 -0400

    fixed chocobar stuff

--- scene_2.gd
+++ scene_2.gd
@@ -1,5 +1,4 @@
 extends Node2D
-var chocobar = preload("res://chocobar.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -12,10 +11,9 @@ func _process(delta):
 
 func _on_chocobar_timer_timeout():
 	randomize()
-	var choco = chocobar.instance()
-	choco.position = Vector2(randf_range(180, 770), randf_range(70, 530))
-	choco.connect("hit", self, "_oh_hit_choco")
+	var choco = preload("res://chocobar.tscn").instantiate()
 	add_child(choco)
+	choco.position = Vector2(randf_range(180, 770), randf_range(70, 490))
 
 func _on_hit_choco():
 		print("hello")
