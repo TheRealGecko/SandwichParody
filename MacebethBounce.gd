ce3c0b8444b369be6d25b9a7b2dc1349b762a313
brrr <65472014+brrrb@users.noreply.github.com>
2023-04-09 21:21:00 -0400

    cat bounce

---
+++ MacebethBounce.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+func _ready():
+	set_velocity(Vector2(800, 250))
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta )
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+		
+func _on_area_2d_body_entered(body):
+	if "Player" in body.name:
+		get_parent().get_node("Player").hp -= 1
+		print("yes")
