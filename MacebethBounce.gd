84eb0ffa0da3714a3f1a4d1646a5c60b3188afb6
TheRealGecko <88910981+TheRealGecko@users.noreply.github.com>
2023-04-09 23:20:40 -0400

    added stage 2 start dialogue

--- MacebethBounce.gd
+++ MacebethBounce.gd
@@ -8,6 +8,6 @@ func _physics_process(delta):
 		velocity = velocity.bounce(collision_info.get_normal())
 		
 func _on_area_2d_body_entered(body):
-	if "Player" in body.name:
+	if "Player" in body.name && get_parent().isDia == false:
 		get_parent().get_node("Player").hp -= 1
 
--- scene_2.gd
+++ scene_2.gd
@@ -2,18 +2,59 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_process_input(true)
+	add_user_signal("next_dia")
+	$Radio.stream = preload("res://res/sound/calm.ogg")
+	$Radio.play()
+	start_dia()
+
+var isDia = true
+
+func start_dia():
+	$Dia.visible = true
+	$chocobar.visible = false
+	for d in sDia:
+		$Dia/Name.text = d[0]
+		$Dia/Text.text = d[1]
+		await Signal(self, "next_dia")
+	$Dia.visible = false
+	$Radio.stream = preload("res://res/sound/fight.ogg")
+	$Radio.play()
+	$chocobar.visible = true
+	true
+	isDia = false
+
+var sDia = [
+	["Paul","Here, the great Macbeth resides. Don’t worry though, she’s not as monstrous
+	as that Macbeth you read about in Grade 11 English."],
+	["Paul","She’s just a tiny lil cat that loves snacking on all kinds of delicacies. :D"],
+	["Paul","By the way, she’s actually a long-lived vampire originating from the 1600s,
+	a rare being indeed!"],
+	["Macbeth","Meowf"],
+	["Batdoon","Awwwww, what a cute cat!"],
+	["Batdoon","(She reminds me of my own lovely cattos at home. Oh! How I miss them so!
+	How could I let myself get led on by some strange old man? I would do anything to
+	just go back home and cuddle with my adorable fluff balls!)"],
+	["Macbeth","*tail slap*"],
+	["Batdoon","!?!?"],
+	["Paul","Oh I forgot to mention but, Macbeth has no arms or legs, just one tadpole tail. :>"],
+	["Batdoon","..."],
+	["Paul","Keep in mind to collect chocolate🍫 to pacify Macbeth!"],
+	["Batdoon","So just like Youmax? Why are these creatures so simple-minded? 😔"]
+]
 
 var Player = preload("res://player.tscn").instantiate()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("enter") && $Dia.visible == true:
+		emit_signal("next_dia")
 
 func _on_chocobar_timer_timeout():
-	randomize()
-	var choco = preload("res://chocobar.tscn").instantiate()
-	add_child(choco)
-	choco.position = Vector2(randf_range(180, 770), randf_range(70, 490))
+	if isDia == false:
+		randomize()
+		var choco = preload("res://chocobar.tscn").instantiate()
+		add_child(choco)
+		choco.position = Vector2(randf_range(180, 770), randf_range(70, 490))
 
 
 func _on_restart_timer_2_timeout():
@@ -22,8 +63,6 @@ func _on_restart_timer_2_timeout():
 func _win_game():
 	$chocobarTimer.stop()
 	$happydance.play("run")
-
-
 	
 
 func _game_over():
